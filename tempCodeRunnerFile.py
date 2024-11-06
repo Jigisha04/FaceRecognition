68f1197f490e3d01dd0911eaf0029c47224c101f
Resham <reshambharti145@gmail.com>
2024-11-06 23:57:35 +0530

    initial commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+@app.route('/video')
+# def video():
+#     return Response(generate_frames(path_x='../Videos/bikes.mp4'), mimetype='multipart/x-mixed-replace; boundary=frame')
+#     #return Response(generate_frames(path_x=0), mimetype='multipart/x-mixed-replace; boundary=frame')
